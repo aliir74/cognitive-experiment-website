5b28189908e2aa49c9331779fe270974aa9e146b
Ali Irani <ali.irani@cafebazaar.ir>
2020-07-24 12:06:52 +0430

    Add bootstrap-vue to project

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 import './filters'
+import './custom.scss'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
