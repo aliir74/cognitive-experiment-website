559384d9c01f0ffa318435d2ebf8df32b0852d15
Ali Irani <ali.irani@cafebazaar.ir>
2020-07-26 17:48:31 +0430

    Add apis to flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,55 @@
 import os
 from flask import Flask, current_app, send_file
-
+from flask import jsonify, request
 from .api import api_bp
 from .client import client_bp
+from flask_pymongo import PyMongo
+import random
+
 
 app = Flask(__name__, static_folder='../dist/static')
+app.config['MONGO_DBNAME'] = 'cognitive'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/cognitive'
+mongo = PyMongo(app)
+
 app.register_blueprint(api_bp)
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/cognitive"
+# mongo = PyMongo(app)
 # app.register_blueprint(client_bp)
 
 from .config import Config
 app.logger.info('>>> {}'.format(Config.FLASK_ENV))
+
+@app.route('/texts', methods=['GET'])
+def get_texts():
+    res = []
+    for t in mongo.db.text.find():
+        res.append({'name': t['name'], 'text': t['text']})
+    return jsonify({'result': res})
+
+@app.route('/register', methods=['POST'])
+def register():
+    form_data = request.form
+    god_numbers = random.shuffle([x*10 for x in range(0,10)])
+    values = [100, 200, 400]
+    god_value = values[random.randint(0, 2)]
+    mongo.db.users.insert_one({'email': form_data.get('email'),
+                               'name': form_data.get('name'),
+                               'mobile': form_data.get('mobile'),
+                               'god_numbers': god_numbers,
+                               'god_value': god_value})
+    return jsonify({'result': {
+        'god_numbers': god_numbers,
+        'god_value': god_value
+    }})
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.get_json()
+    user = mongo.db.users.find({'email': data['email']})
+    user['value'] = data['value']
+    user['judge_value'] = data['judge_value']
+    user['iri_value'] = data['iri_value']
 
 @app.route('/')
 def index_client():
