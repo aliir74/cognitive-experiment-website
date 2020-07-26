f5cbaf9ea685b58ad292fd65e65dc4f86523d3e2
Ali Irani <ali.irani@cafebazaar.ir>
2020-07-26 12:59:07 +0430

    Some style fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,14 +4,14 @@
       <b-col></b-col>
 
       <b-col cols="8">
-        <b-row class="vh-50">
+        <b-row class="vh-50" style="min-height: 50vh !important;">
           <b-col cols="2"></b-col>
           <b-col>
             <PersonalInformation msg="Welcome to my cognitive experiment" v-show="step == 0"/>
             <Explain msg="Welcome to my cognitive experiment" v-show="step == 1"/>
             <Decision v-bind:msg="decision_msg" v-show="step == 2"/>
-            <Decision v-for="(it, index) in supervisor_numbers" v-bind:msg="supervisor_msg.replace('{0}', it)" :key="index"
-                      v-show="step == 3+index" v-bind:supervisor="true"></Decision>
+            <Decision v-for="(it, index) in supervisor_numbers" v-bind:msg="supervisor_msg.replace('{0}', it)"
+                      :key="index" v-show="step == 3+index" v-bind:supervisor="true"></Decision>
           </b-col>
           <b-col cols="2"></b-col>
         </b-row>
@@ -21,7 +21,7 @@
             <b-button variant="success" v-on:click="step += 1">بعدی</b-button>
           </b-col>
           <b-col>
-            <b-button variant="danger" v-on:click="step -= 1" :disabled="!(step <= 2 && step >= 1)">قبلی</b-button>
+            <b-button variant="danger" v-on:click="step -= 1" :disabled="false && !(step <= 2 && step >= 1)">قبلی</b-button>
           </b-col>
           <b-col></b-col>
         </b-row>
@@ -44,7 +44,7 @@ export default {
   data () {
     return {
       decision_msg: '        اگر فقط یکبار فرصت داشته باشین که پول رو تقسیم کنین، چه مقدار از پول رو به طرف مقابل می‌دین؟',
-      supervisor_msg: 'حالا فرض کنین شما ناظر آزمایش قبل هستین و ۱۰۰ هزار تومن پول در اختیار دارین. اگر نفر اول {0} هزار تومن به نفر دوم داده باشه شما به نفر اول چقدر پول میدین؟',
+      supervisor_msg: 'حالا فرض کنین شما ناظر آزمایش قبل هستین و 100 هزار تومن پول در اختیار دارین. اگر نفر اول {0} هزار تومن به نفر دوم داده باشه شما به نفر اول چقدر پول میدین؟',
       supervisor_numbers: [30, 60, 100, 5]
     }
   },
@@ -68,5 +68,7 @@ export default {
 </script>
 
 <style lang="scss">
-
+  .card-body {
+    min-height: 40vh !important;
+  }
 </style>
